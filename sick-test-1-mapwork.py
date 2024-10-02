f161cda8b5a502586bddae055106a26a28cb4544
Barend van der Merwe <131867350+barend-van-der-merwe@users.noreply.github.com>
2024-10-02 12:13:34 +0200

    Update sick-test-1-mapwork.py

--- sick-test-1-mapwork.py
+++ sick-test-1-mapwork.py
@@ -118,10 +118,10 @@ if image is not None:
                                fontScale=1, color=(0, 0, 255), thickness=1)
         #text on map
         final_img = cv.putText(img=warped_img, text=f'{int(st.session_state.chk7) + int(st.session_state.chk8)}',
-                               org=(330,568), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                               org=(335,818), fontFace=cv.FONT_HERSHEY_SIMPLEX,
                                fontScale=1, color=(0, 0, 255), thickness=1)
         final_img = cv.putText(img=warped_img, text=f'{int(st.session_state.chk9) + int(st.session_state.chk10)}',
-                               org=(455,505), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                               org=(556,754), fontFace=cv.FONT_HERSHEY_SIMPLEX,
                                fontScale=1, color=(0, 0, 255), thickness=1)
         final_img = cv.putText(img=warped_img, text=f'{grade}', org=(550, 150),
                                fontFace=cv.FONT_HERSHEY_SIMPLEX,
