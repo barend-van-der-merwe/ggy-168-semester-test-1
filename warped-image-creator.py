61960199a1aa385933961aed4ebe22cf5a412ed6
Barend van der Merwe <131867350+barend-van-der-merwe@users.noreply.github.com>
2024-11-21 12:20:26 +0200

    Add files via upload

---
+++ warped-image-creator.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import cv2 as cv
+import pandas as pd
+import numpy as np
+from PIL import Image
+import io
+import re
+
+st.title("Warped Image Creator")
+
+st.subheader("Select Student Submission")
+image = st.file_uploader("Select the student submission", type = ["png", "jpg"])
+
+if image is not None:
+    img_bytes = np.asarray(bytearray(image.read()), dtype=np.uint8)
+    img = cv.imdecode(img_bytes, 1)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
+    parameters =  cv.aruco.DetectorParameters()
+    detector = cv.aruco.ArucoDetector(dictionary, parameters)
+    corners, ids, _ = detector.detectMarkers(img)
+    ids = np.concatenate(ids, axis=0).tolist()
+    WIDTH = 712
+    HEIGHT = 972
+    aruco_top_left = corners[ids.index(0)]
+    aruco_top_right = corners[ids.index(1)]
+    aruco_bottom_right = corners[ids.index(2)]
+    aruco_bottom_left = corners[ids.index(3)]
+    point1 = aruco_top_left[0][0]
+    point2 = aruco_top_right[0][1]
+    point3 = aruco_bottom_right[0][2]
+    point4 = aruco_bottom_left[0][3]
+    working_image = np.float32([[point1[0], point1[1]],
+                                [point2[0], point2[1]],
+                                [point3[0], point3[1]],
+                                [point4[0], point4[1]]])
+    working_target = np.float32([[0, 0],
+                                 [WIDTH, 0],
+                                 [WIDTH, HEIGHT],
+                                 [0, HEIGHT]])
+    transformation_matrix = cv.getPerspectiveTransform(working_image, working_target)
+    warped_img = cv.warpPerspective(img_gray, transformation_matrix, (WIDTH, HEIGHT))
+
+    st.image(warped_img)
+
+    #final_img_rgb = cv.cvtColor(final_img, cv.COLOR_BGR2RGB)
+    final_img_pil = Image.fromarray(warped_img)
+    buffer = io.BytesIO()
+    final_img_pil.save(buffer, format="PNG")
+    st.download_button(label=f"Download Warped Image", data=buffer, file_name="warped-image.png", mime="image/jpeg")
